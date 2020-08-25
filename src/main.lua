c40397ab6928952daf9ef2fd8334aa38477bbe1b
Kumodatsu <dragonkumo@gmail.com>
2020-08-26 00:49:37 +0200

    Add version command

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,18 @@
 local addon_name, SL = ...
 
+-- Version command
+local show_version = function()
+    local author  = GetAddOnMetadata("ShoppingList", "author")
+    local title   = GetAddOnMetadata("ShoppingList", "title")
+    local version = GetAddOnMetadata("ShoppingList", "version")
+    SL.Print("%s's %s, version %s", author, title, version)
+end
+
+SL.Command.add_cmd("version", show_version, [[
+"/sl version"
+> "/sl version" shows information about the currently loaded version of ShoppingList.
+]])
+
 -- Global addon getter
 SL_API = SL
 
